6a0312721d0e9ec549c51886c32c990e48133374
Leonardo Vizcaya Savchenko <lvizcaya@arpixnet.com>
2026-01-17 18:52:43 -0500

    Update example-content.vue

--- app/pages/example-content.vue
+++ app/pages/example-content.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-    // const { data: content, pending } = await useAsyncData(() => queryCollection('content').path('/example-content').first())
+    const { data: content, pending } = await useAsyncData(() => queryCollection('content').path('/example-content').first())
 </script>
 
 <template>
     <div class="w-full min-h-screen p-10">
-        <!-- <div v-if="pending" class="p-4">
+        <div v-if="pending" class="p-4">
             <div class="skeleton h-8 mb-4"></div>
             <div class="skeleton h-4 mb-2"></div>
             <div class="skeleton h-4 w-3/4"></div>
@@ -18,7 +18,7 @@
             <p>No content found.</p>
             <p class="mt-2 font-semibold">Install @nuxt/content by running</p>
             <code class="w-fit mt-2 block bg-black text-white py-4 px-6 rounded-lg">npm run add:content</code>
-        </div> -->
+        </div>
     </div>
 </template>
 
